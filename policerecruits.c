2b50f853e901a5e83784c3950d5838a9ee0194b7
Andre Rodríguez <AndreRdz7@users.noreply.github.com>
2018-08-15 16:55:04 -0500

    policerecruits.c

--- policerecruits.c
+++ policerecruits.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
+  GNU nano 2.9.7                   policerecruits.c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+main(int argc, char const *argv[]){
+    int number_of_events,i;
+    int untreated_crimes = 0;
+    int police_officers = 0;
+    scanf("%d",&number_of_events);
+    int cases[number_of_events];
+    for(i = 0; i < number_of_events; i++){
+        scanf("%d",&cases[i]);
+        if (cases[i] < 0){
+            if(police_officers <= 0){
+                untreated_crimes += 1;
+            }
+            else {
+                police_officers--;
+            }
+        }
 #include <stdlib.h>
 
 main(int argc, char const *argv[]){
