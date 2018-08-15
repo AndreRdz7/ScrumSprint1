3cb1304c4d829861aafeb7a220908ccf708a1624
Alkiva <fernanda22mr@gmail.com>
2018-08-15 15:20:30 -0500

    add problem

---
+++ vanya_f.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class vanya_f{
+	public static void main(String []args){
+	Scanner sc=new Scanner(System.in);
+		int n,h,res=0,p;
+		n=sc.nextInt();
+		h=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			p=sc.nextInt();
+			if(p<h)
+				res++;
+			else
+				res=res+2;
+		}
+		System.out.println(res);
+
+	}
+}
